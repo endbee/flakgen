551d42f7e06b05abe9e288ad3b8b584141bd2bff
Fin Bießler <fin.biessler@check24.de>
2023-10-24 12:48:11 +0200

    make code better expendable

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
-from generation.random_api import generate_flaky_summation_test_and_function_pair
+from generation.random_api import generate_randapi_flaky_summation_test_and_function_pair
 import os
 import astor
 
 def main():
+    flakiness_rel = 0.5
+    summation_depth = 5
 
-    func_tree, test_tree = generate_flaky_summation_test_and_function_pair(5, 0.5)
+    flakiness_category_generators = {
+        'randapi_summation': generate_randapi_flaky_summation_test_and_function_pair
+    }
 
-    if not os.path.exists('bin'):
-        os.makedirs('bin')
+    for key in flakiness_category_generators:
+        if key == 'randapi_summation':
+            func_tree, test_tree = flakiness_category_generators[key](summation_depth, flakiness_rel)
 
-    f = open("bin/test_sample1.py", "w")
-    f.write(astor.to_source(func_tree))
-    f.write("\n")
-    f.write(astor.to_source(test_tree))
-    f.close()
+        if not os.path.exists('bin'):
+            os.makedirs('bin')
+
+        f = open("bin/test_sample1.py", "w")
+        f.write(astor.to_source(func_tree))
+        f.write("\n")
+        f.write(astor.to_source(test_tree))
+        f.close()
 
     stream = os.popen('cd bin && pytest && cd ..')
     output = stream.read()
