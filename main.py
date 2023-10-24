517cd30d4c6643a0c5025bf3a5b5c3c0308af9e8
Fin Bießler <fin.biessler@check24.de>
2023-10-24 11:53:06 +0200

    make flaky test generation a bit more scientifically

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from generation.generation import generate_sum_function, generate_sum_test
+from generation.generation import generate_sum_function, generate_sum_test, generate_flaky_summation, generate_summation_test
 import os
 import astor
 
 def main():
-    func_string = astor.to_source(generate_sum_function())
-    test_string = astor.to_source(generate_sum_test())
+
+    func_string = astor.to_source(generate_flaky_summation(5))
+    test_string = astor.to_source(generate_summation_test(5))
 
     if not os.path.exists('bin'):
         os.makedirs('bin')
@@ -18,6 +19,7 @@ def main():
     stream = os.popen('cd bin && pytest && cd ..')
     output = stream.read()
     print(output)
-    
+
+
 if __name__ == "__main__":
     main()
