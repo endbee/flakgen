82136d8064cee582ebeaa26df559afbef3997a71
Fin Bießler <fin.biessler@check24.de>
2023-11-02 14:26:04 +0100

    move writing import statement from abc to concrete file writing class

--- file_writing/test_file_writer.py
+++ file_writing/test_file_writer.py
@@ -4,5 +4,13 @@ from .file_writer import FileWriter
 class TestFileWriter(FileWriter):
     FILE_PATH_POSTFIX = "_test"
 
+    # When instantiating the TestFileWrite it first writes the import-Statement that imports the functions to be tested
+    def __init__(self, module_name):
+        super().__init__(module_name)
+        self.file_handler.write("import " + self.module_name)
+        self.file_handler.write("\n")
+        self.file_handler.write("\n")
+        self.file_handler.write("\n")
+
     def get_file_path(self):
         return "testsuite/" + self.module_name + self.FILE_PATH_POSTFIX + ".py"
